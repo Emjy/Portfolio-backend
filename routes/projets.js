18a112cb3ad42acbdd1aa68449ed07d9fa0a35a8
Emilien <emiliengir@gmail.com>
2024-04-04 16:06:05 +0200

    ajout route projets unique

--- routes/projets.js
+++ routes/projets.js
@@ -4,12 +4,26 @@ var router = express.Router();
 const Projet = require("../models/projets");
 
 
-/* GET home page. */
+// Get de tous les projets 
 router.get("/", async (req, res) => {
 
     try {
         const projets = await Projet.find();
+
         res.json({ result: true, projets });
+    } catch (error) {
+        res.json({ result: false, error });
+    }
+
+});
+
+// Get d'un projet 
+router.get("/:id", async (req, res) => {
+
+    try {
+        const projet = await Projet.findById(req.params.id);
+
+        res.json({ result: true, projet });
     } catch (error) {
         res.json({ result: false, error });
     }
